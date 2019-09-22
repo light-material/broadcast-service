1051324feea6e8cdd423984b1f8088c9b6ee8728
DJames <davejames1109@gmail.com>
2019-09-23 00:48:37 +0800

    Added project structure

---
+++ src/main/java/com/material/light/broadcastservice/BroadcastServiceApplication.java
@@ -0,0 +1,13 @@
+package com.material.light.broadcastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadcastServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadcastServiceApplication.class, args);
+    }
+
+}
